b28acfd89792b4d17830ba05b057dd2fe6458fff
1Prestes <dijango_ilha@hotmail.com>
2020-06-29 19:25:47 -0300

    Nodemon configured and Auth controller created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const authController = require('./constrollers/auth');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.json('Oi');
-})
+});
+
+app.use('/auth', authController);
 
 app.listen(3001, () => {
     console.log('SERVER RUNNING!');
