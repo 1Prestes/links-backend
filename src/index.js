c50f76becdd38e950f5bc9dac81e09d0cb39d190
1Prestes <dijango_ilha@hotmail.com>
2020-07-01 20:53:51 -0300

    Links CRUD

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ const db = require('./models');
 const response = require('./middlewares/response');
 
 const authController = require('./constrollers/auth');
-
+ const linkController = require('./constrollers/link');
 const app = express();
 
 app.use(response);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.json('Oi');
+    res.json('App Running');
 });
 
 app.use('/auth', authController);
+app.use('/link', linkController);
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
