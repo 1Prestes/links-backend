eb96cc1d30a1d335a338a127d1977b4232b59811
1Prestes <dijango_ilha@hotmail.com>
2020-07-01 18:58:06 -0300

    JWT implemented.

--- src/constrollers/auth.js
+++ src/constrollers/auth.js
@@ -1,26 +1,41 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const { Account } = require('../models');
-const { accountSignUp } = require('../validators/account');
+const { accountSignUp, accountSignIn } = require('../validators/account');
 const { getMessage } = require('../helpers/validator');
+const { generatJwt, generatRefreshJwt } = require('../helpers/jwt');
 
 const router = express.Router();
 
-router.get('/sign-in', (req, res) => {
-    return res.jsonOK(null);
-});
-
-router.get('/sign-up', accountSignUp, async (req, res) => {
+router.post('/sign-in', accountSignIn, async (req, res) => {
     const { email, password } = req.body;
     const account = await Account.findOne({ where: { email } });
 
-    if (account) {
-        return res.jsonBadRequest(null, getMessage('account.signup.email_exists'));
-    }
+    // Valid password
+    const match = account ? bcrypt.compareSync(password, account.password) : null;
+
+    if (!match) return res.jsonBadRequest(null, getMessage('account.signin.invalid'));
+
+    const token = generatJwt({ id: account.id });
+    const refreshToken = generatRefreshJwt({ id: account.id });
+
+    return res.jsonOK(account, null, { token, refreshToken });
+});
+
+router.post('/sign-up', accountSignUp, async (req, res) => {
+    const { email, password } = req.body;
+    const account = await Account.findOne({ where: { email } });
+
+    if (account) return res.jsonBadRequest(null, getMessage('account.signup.email_exists'));
+
 
     const hash = bcrypt.hashSync(password, 10);
     const newAccount = await Account.create({ email, password: hash });
-    return res.jsonOK(newAccount, getMessage('account.signup.success'));
+
+    const token = generatJwt({ id: newAccount.id });
+    const refreshToken = generatRefreshJwt({ id: newAccount.id });
+
+    return res.jsonOK(newAccount, getMessage('account.signup.success'), { token, refreshToken });
 });
 
 module.exports = router;
---
+++ src/helpers/jwt.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+
+const tokenPrivateKey = process.env.JWT_TOKEN_PRIVATE_KEY;
+const refreshTokenPrivateKey = process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY;
+const options = { expiresIn: '30 minutes' }
+const refreshOptions = { expiresIn: '1 days' }
+
+const generatJwt = (payload) => {
+    return jwt.sign(payload, tokenPrivateKey, options)
+}
+
+const generatRefreshJwt = (payload) => {
+    return jwt.sign(payload, refreshTokenPrivateKey, options)
+}
+
+const verifyJwt = (token) => {
+    return jwt.verify(token, tokenPrivateKey);
+}
+
+const verifyRefreshJwt = (token) => {
+    return jwt.verify(token, refreshTokenPrivateKey);
+}
+
+module.exports = { generatJwt, generatRefreshJwt, verifyJwt, verifyRefreshJwt };
